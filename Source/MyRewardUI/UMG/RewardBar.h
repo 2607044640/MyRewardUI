ae2661d1cc905e6ff5a37f78fed54512b9a88320
2607044640 <2607044640@qq.com>
2024-06-04 15:41:56 +0800

    Initial commit

---
+++ Source/MyRewardUI/UMG/RewardBar.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RewardBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYREWARDUI_API URewardBar : public UUserWidget
+{
+	GENERATED_BODY()
+};
